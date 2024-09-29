37824c9f304021479fce614192fb2562102bdf27
MahaGit1233 <maharush5409@gmail.com>
2024-09-29 14:07:49 +0530

    task1 commit

---
+++ task1.js
@@ -0,0 +1,13 @@
+const heading=document.getElementById('main-heading');
+heading.textContent='Fruit World';
+heading.style.color='orange';
+
+const divHead=document.getElementById('header');
+divHead.style.backgroundColor='green';
+divHead.style.borderBottom='3px solid orange';
+
+const basket=document.getElementById('basket-heading');
+basket.style.color='green';
+
+const regards=document.getElementById('thanks');
+regards.innerHTML='<p>Please visit us again</p>';
